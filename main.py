e5bddee46b9b2471e1e53d34793727b1a43a7f4e
vivek6391 <vivek6391@gmail.com>
2016-04-15 20:46:55 -0700

    updated main.py to add network layers

--- main.py
+++ main.py
@@ -15,6 +15,89 @@ from theano.printing import Print as pp
 def str2bool(v):
     return v.lower() in ('yes', 'true', 't', '1')
 
+import warnings
+warnings.filterwarnings('ignore', '.*topo.*')
+
+
+class InnerProductLayer(lasagne.layers.MergeLayer):
+
+    def __init__(self, incomings, nonlinearity=None, **kwargs):
+        super(InnerProductLayer, self).__init__(incomings, **kwargs)
+        self.nonlinearity = nonlinearity
+        if len(incomings) != 2:
+            raise NotImplementedError
+
+    def get_output_shape_for(self, input_shapes):
+        return input_shapes[0][:2]
+
+    def get_output_for(self, inputs, **kwargs):
+        M = inputs[0]
+        u = inputs[1]
+        output = T.batched_dot(M, u)
+        if self.nonlinearity is not None:
+            output = self.nonlinearity(output)
+        return output
+
+
+class BatchedDotLayer(lasagne.layers.MergeLayer):
+
+    def __init__(self, incomings, **kwargs):
+        super(BatchedDotLayer, self).__init__(incomings, **kwargs)
+        if len(incomings) != 2:
+            raise NotImplementedError
+
+    def get_output_shape_for(self, input_shapes):
+        return (input_shapes[1][0], input_shapes[1][2])
+
+    def get_output_for(self, inputs, **kwargs):
+        return T.batched_dot(inputs[0], inputs[1])
+
+
+class SumLayer(lasagne.layers.Layer):
+
+    def __init__(self, incoming, axis, **kwargs):
+        super(SumLayer, self).__init__(incoming, **kwargs)
+        self.axis = axis
+
+    def get_output_shape_for(self, input_shape):
+        return input_shape[:self.axis] + input_shape[self.axis+1:]
+
+    def get_output_for(self, input, **kwargs):
+        return T.sum(input, axis=self.axis)
+
+
+class TemporalEncodingLayer(lasagne.layers.Layer):
+
+    def __init__(self, incoming, T=lasagne.init.Normal(std=0.1), **kwargs):
+        super(TemporalEncodingLayer, self).__init__(incoming, **kwargs)
+        self.T = self.add_param(T, self.input_shape[-2:], name="T")
+
+    def get_output_shape_for(self, input_shape):
+        return input_shape
+
+    def get_output_for(self, input, **kwargs):
+        return input + self.T
+
+
+class TransposedDenseLayer(lasagne.layers.DenseLayer):
+
+    def __init__(self, incoming, num_units, W=lasagne.init.GlorotUniform(),
+                 b=lasagne.init.Constant(0.), nonlinearity=lasagne.nonlinearities.rectify,
+                 **kwargs):
+        super(TransposedDenseLayer, self).__init__(incoming, num_units, W, b, nonlinearity, **kwargs)
+
+    def get_output_shape_for(self, input_shape):
+        return (input_shape[0], self.num_units)
+
+    def get_output_for(self, input, **kwargs):
+        if input.ndim > 2:
+            input = input.flatten(2)
+
+        activation = T.dot(input, self.W.T)
+        if self.b is not None:
+            activation = activation + self.b.dimshuffle('x', 0)
+        return self.nonlinearity(activation)
+	
 class Model:
 
     def __init__(self, train_file, test_file, batch_size=32, embedding_size=20, max_norm=40, lr=0.01, num_hops=3, adj_weight_tying=True, linear_start=True, **kwargs):
